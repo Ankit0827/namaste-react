fdfc0c03862c92414611bd2a03f09805d918613a
Ankit Verma <a.verma0827@gmail.com>
2023-07-17 17:55:10 +0530

    .

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const heading=React.createElement("div",{id:"parent"},[
         
     ]),
     React.createElement("div",{id:"child2"},[
-        React.createElement("h2",{id:"heading2"},"I am heading two")
+        React.createElement("h2",{id:"heading2"},"I am heading two"),
+        React.createElement("p",{id:"heading2"},"I am paragraph tag"),
+
     ]),
 
 ]);
